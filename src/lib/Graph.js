321752c2fea57ab5faccd4600bd3d1d9b6564258
RC <rc.chandru@gmail.com>
2017-09-27 15:55:41 +0530

    Graph DS tailor-made for the game rules

--- src/lib/Graph.js
+++ src/lib/Graph.js
@@ -1,17 +1,25 @@
 export default class Graph {
-  _vertices = []
-  _edges = {}
+  _vertices = new Set()
+  _verticalEdges = {}
+  _horizontalEdges = {}
   _values = {}
   _inferredPath = []
 
-  add = (vertex1, vertex2) => {
-    this.vertices.push(vertex1, vertex2)
-    this.addUndirectedEdges(vertex1, vertex2)
+  addVerticalEdge = (vertex1, vertex2) => {
+    this._vertices.add(vertex1)
+    this._vertices.add(vertex2)
+    this.addUndirectedEdges(this.verticalEdges, vertex1, vertex2)
   }
 
-  addUndirectedEdges = (vertex1, vertex2) => {
-    this.edges[vertex1] ? this.edges[vertex1].push(vertex2) : (this.edges[vertex1] = [vertex2])
-    this.edges[vertex2] ? this.edges[vertex2].push(vertex1) : (this.edges[vertex2] = [vertex1])
+  addHorizontalEdge = (vertex1, vertex2) => {
+    this._vertices.add(vertex1)
+    this._vertices.add(vertex2)
+    this.addUndirectedEdges(this.horizontalEdges, vertex1, vertex2)
+  }
+
+  addUndirectedEdges = (edges, vertex1, vertex2) => {
+    edges[vertex1] ? edges[vertex1].push(vertex2) : (edges[vertex1] = [vertex2])
+    edges[vertex2] ? edges[vertex2].push(vertex1) : (edges[vertex2] = [vertex1])
   }
 
   adjacent = (vertex1, vertex2) => {
@@ -30,33 +38,57 @@ export default class Graph {
     return this.values[vertex]
   }
 
+  isSingleHop = (fromVertex, toVertex) => {
+  	return this.path(fromVertex, toVertex).length === 3
+	}
+
   path = (fromVertex, toVertex) => {
     this._inferredPath = []
-    this.traverse(fromVertex, toVertex, [fromVertex])
+    this.traverse(fromVertex, toVertex, this.horizontalEdges, [fromVertex])
+		if (this._inferredPath.length === 0){
+			this.traverse(fromVertex, toVertex, this.verticalEdges, [fromVertex])
+		}
     return this._inferredPath
   }
 
-  traverse = (fromVertex, toVertex, path) => {
-    console.log('path: ' + path)
+  traverse = (fromVertex, toVertex, edges, path) => {
+    // console.log('path: ' + path)
     if (fromVertex === toVertex) {
       this._inferredPath = path
+			return
     }
-    const directedEdges = this.edges[fromVertex] && this.edges[fromVertex].filter(v => !path.includes(v))
-    console.log('edges: ' + directedEdges)
+    const directedEdges = edges[fromVertex] && edges[fromVertex].filter(v => !path.includes(v))
+    // console.log('edges: ' + directedEdges)
     if (directedEdges && directedEdges.length > 0) {
       directedEdges.forEach(v => {
-        return this.traverse(v, toVertex, [...path, v])
+        return this.traverse(v, toVertex, edges, [...path, v])
       })
     }
-    return undefined
   }
 
   get vertices() {
-    return this._vertices
+    return [...this._vertices]
+  }
+
+  get horizontalEdges() {
+    return this._horizontalEdges
+  }
+
+  get verticalEdges() {
+    return this._verticalEdges
   }
 
   get edges() {
-    return this._edges
+    // deep merge both horizontal and vertical edges
+    const edges = Object.assign({}, this.verticalEdges)
+    Object.keys(this.horizontalEdges).forEach(v => {
+      if (edges[v]) {
+        edges[v] = [...edges[v], ...this.horizontalEdges[v]]
+      } else {
+        edges[v] = this.horizontalEdges[v]
+      }
+    })
+    return edges
   }
 
   get values() {
--- src/lib/Graph.test.js
+++ src/lib/Graph.test.js
@@ -3,54 +3,97 @@ import Graph from './Graph'
 let g = null
 
 describe('Graph', function() {
-    beforeEach(function() {
-        g = new Graph()
-        g.add(1, 2)
+  beforeEach(function() {
+    g = new Graph()
+  })
+
+  describe('#add edges', () => {
+    it('should add vertical edges edges edges along with vertices', () => {
+      g.addVerticalEdge(1, 2)
+      g.addVerticalEdge(2, 3)
+      expect(g.vertices).toEqual([1, 2, 3])
+      expect(g.edges).toEqual({1: [2], 2: [1, 3], 3: [2]})
     })
 
-    describe('#add', () => {
-        it('should add the edges along with vertices', () => {
-            expect(g.vertices).toEqual([1, 2])
-            expect(g.edges).toEqual({1: [2], 2: [1]})
-        })
+    it('should add horizontal edges edges edges along with vertices', () => {
+      g.addVerticalEdge(1, 2)
+      g.addHorizontalEdge(2, 3)
+      expect(g.vertices).toEqual([1, 2, 3])
+      expect(g.edges).toEqual({1: [2], 2: [1, 3], 3: [2]})
+    })
+  })
+
+  describe('#adjacent', () => {
+    it('should return true if v1 and v2 are adjacent', () => {
+      g.addVerticalEdge(1, 2)
+      g.addVerticalEdge(2, 3)
+      expect(g.adjacent(1, 2)).toBeTruthy()
+      expect(g.adjacent(2, 3)).toBeTruthy()
+      expect(g.adjacent(1, 3)).toBeFalsy()
+    })
+  })
+
+  describe('#neighbours', () => {
+    it('should return all adjacent vertices given a vertex', () => {
+      g.addVerticalEdge(1, 2)
+      g.addHorizontalEdge(2, 3)
+      expect(g.neighbours(2)).toEqual([1, 3])
+      expect(g.neighbours(1)).toEqual([2])
+      expect(g.neighbours(3)).toEqual([2])
+      expect(g.neighbours(4)).toEqual(undefined)
+    })
+  })
+
+  describe('Values', () => {
+    it('should assign and retrieve value to a vertex', () => {
+      g.addVerticalEdge(1, 2)
+      g.setValue(1, 'abc')
+      expect(g.valueOf(1)).toEqual('abc')
+      expect(g.valueOf(2)).toEqual(undefined)
+    })
+  })
+
+  describe('#path', () => {
+		/*
+			/         1
+			/      /  |  \
+			/     2 - 3 - 4
+			/    /    |   \
+			/    5 -- 6 -- 7
+			/
+			 */
+
+
+		beforeEach(() => {
+			g.addVerticalEdge(1, 2)
+			g.addVerticalEdge(1, 3)
+			g.addVerticalEdge(1, 4)
+			g.addHorizontalEdge(2, 3)
+			g.addHorizontalEdge(3, 4)
+			g.addVerticalEdge(2, 5)
+			g.addVerticalEdge(3, 6)
+			g.addVerticalEdge(4, 7)
+			g.addHorizontalEdge(5, 6)
+			g.addHorizontalEdge(6, 7)
     })
 
-    describe('#adjacent', () => {
-        it('should return true if v1 and v2 are adjacent', () => {
-            g.add(2, 3)
-            expect(g.adjacent(1, 2)).toBeTruthy()
-            expect(g.adjacent(2, 3)).toBeTruthy()
-            expect(g.adjacent(1, 3)).toBeFalsy()
-        })
+    it('should return the path from v1 to v2 if present', () => {
+      expect(g.path(1, 5)).toEqual([1, 2, 5])
+      expect(g.path(1, 4)).toEqual([1, 4])
+      expect(g.path(2, 4)).toEqual([2, 3, 4])
+      expect(g.path(5, 7)).toEqual([5, 6, 7])
+      expect(g.path(3, 7)).toEqual([3,1,4,7])
     })
 
-    describe('#neighbours', () => {
-        it('should return all adjacent vertices given a vertex', () => {
-            g.add(2, 3)
-            expect(g.neighbours(2)).toEqual([1, 3])
-            expect(g.neighbours(1)).toEqual([2])
-            expect(g.neighbours(3)).toEqual([2])
-            expect(g.neighbours(4)).toEqual(undefined)
-        })
+    it('should find a single hop path from v1 to v2', () => {
+			g.addVerticalEdge(5, 8)
+      expect(g.isSingleHop(1,5)).toBeTruthy()
+      expect(g.isSingleHop(2,4)).toBeTruthy()
+      expect(g.isSingleHop(1,7)).toBeTruthy()
+      expect(g.isSingleHop(2,8)).toBeTruthy()
+      expect(g.isSingleHop(3,7)).toBeFalsy()
     })
 
-    describe('Values', () => {
-        it('should assign and retrieve value to a vertex', () => {
-            g.setValue(1, 'abc')
-            expect(g.valueOf(1)).toEqual('abc')
-            expect(g.valueOf(2)).toEqual(undefined)
-        })
-    })
 
-    describe('#path', () => {
-        it('should return the path from v1 to v2 if present', () => {
-            g.add(1, 3)
-            g.add(2, 4)
-            g.add(4, 5)
-            expect(g.path(1, 5)).toEqual([1, 2, 4, 5])
-            expect(g.path(1, 4)).toEqual([1, 2, 4])
-            expect(g.path(1, 3)).toEqual([1, 3])
-            expect(g.path(3, 5)).toEqual([3, 1, 2, 4, 5])
-        })
-    })
+  })
 })
